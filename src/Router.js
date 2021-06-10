57c55df7659973b696ed1c107d0c8655d6cab060
Ibrahim Banat <ibrahim.banat.97@gmail.com>
2021-06-10 15:03:31 +0300

    finished the video for rooms

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,16 @@
 'use strict';
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 router.get('/', rootHandler);
 
+router.get('/:id', roomHandler);
+
 function rootHandler(req, res) {
   res.send('root is working');
+}
+function roomHandler(req, res) {
+  res.sendFile('room.html', { root: path.join(__dirname, '../public') });
 }
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -9,27 +9,43 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
-const http = require('http');
-const socket = require('socket.io');
 const peer = require('peer');
 const morgan = require('morgan');
 const multer = require('multer');
 const multParse = multer();
+
+const server = require('http').createServer(app);
+const socket = require('socket.io');
+const io = socket(server, {
+  cors: { origin: '*' },
+});
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(multParse.none());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(express.static(path.join(__dirname, '../public')));
 //routes
 app.use(router);
 //catchalls
 app.use('*', notFound);
 app.use(internalError);
 
+io.on('connection', socket => {
+  socket.on('join-room', (roomId, userId) => {
+    console.log(roomId);
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit('user-connected', userId);
+
+    socket.on('disconnect', () => {
+      socket.broadcast.to(roomId).emit('user-disconnected', userId);
+    });
+  });
+});
+
 module.exports = port => {
-  app.listen(port, () => {
+  server.listen(port, () => {
     console.log('Server is up . . . ');
     console.log(`Server is working at http://localhost:${port}`);
   });
