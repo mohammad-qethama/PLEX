8ab722cae31f0eb5ee40f8bb24780ad341c6106d
tamaraalbilleh <tamara2albella@gmail.com>
2021-06-11 20:39:40 +0300

    datacollection and test api

--- src/__tests__/api.test.js
+++ src/__tests__/api.test.js
@@ -1,2 +1,32 @@
 'use strict';
+require('dotenv').config();
+const supergoose =require('@code-fellows/supergoose');
+const {app}=require('../server');
+const {expect}= require('@jest/globals');
+const request= supergoose(app);
+let id;
+describe('api test',()=>{
+  it('should get All Event',async()=>{
+    const res = await request.get('/getEvents');
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual([]);
+  });
+  it('should create new event',async()=>{
+    const res = await request.post('/createEvent').send(
+      {
+        attendance_limit:10,
+        city:'amman',
+        catagories:'art',
+      },
+    );
+    expect(res.status).toEqual(201);
+    expect(res.body.city).toEqual('amman');
+    id=res.body._id;
+  });
+  it('should get event by id ',async()=>{
+    const res= await request.get(`/getEvents/${id}`);
+    expect(res.status).toEqual(200);
+    expect(res.body.city).toEqual('amman');
+  });
+});
 
