73a640ca025002f1d24e7b5cb168956d0946d047
Ibrahim Banat <ibrahim.banat.97@gmail.com>
2021-06-12 15:06:24 +0300

    adding session and cookies

--- src/auth/middlewares/isLogged.js
+++ src/auth/middlewares/isLogged.js
@@ -2,10 +2,10 @@
 
 module.exports = (req, res, next) => {
   try {
-    let token = req.cookies['token'];
+    let token = req.cookies['token'] || req.cookies['session-token'];
     console.log(token);
-    token ? next() : res.redirect('/signin');
+    token ? next() : res.redirect('/signin.html');
   } catch (error) {
-    console.log(error);
+    res.status(403).json('Acess Denied');
   }
 };
