ec8a5e420b4c826d2f6d51d19a7ec095a250eb7f
Ibrahim Banat <55441454+ibrahimBanat@users.noreply.github.com>
2021-06-16 23:34:10 +0300

    Revert "Dev"

--- src/errors/404.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = (req, res, next) => {
-  res.status(404).json({
-    status: 404,
-    message:
-      'seems like beheemoth took the file to the upside down, file does not exist right now',
-  });
-};
